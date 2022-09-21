eb845f6d44e604b5ef9120cd404826ff495eef4d
William Queiroz <william.queiroz@neon.com.br>
2022-09-21 18:16:01 -0300

    chore: setup TypeScript

---
+++ packages/ui/src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>Aluraflix</title>
+          <meta name="description" content="Aluraflix" />
+          <link rel="icon" href="/favicon.svg" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
