726e1151350eeb78ad0daa7fcf868513712e4112
William Queiroz <wnqueiroz@outlook.com>
2022-09-25 13:29:48 -0300

    feat: setup TypeORM

--- packages/boilerplate/src/app.module.ts
+++ packages/boilerplate/src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        synchronize: true,
+        entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+        port: 5432,
+        host: configService.get('TYPEORM_HOST'),
+        username: configService.get('TYPEORM_USERNAME'),
+        password: configService.get('TYPEORM_PASSWORD'),
+        database: configService.get('TYPEORM_DATABASE'),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
