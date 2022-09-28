0c52ed02dba6dbd3301e84584d2d1099abd80501
William Queiroz <wnqueiroz@outlook.com>
2022-09-27 21:22:38 -0300

    feat: create movies entity

--- packages/movies/src/app.module.ts
+++ packages/movies/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
 import { MoviesModule } from './movies/movies.module';
 
 @Module({
--- packages/movies/src/movies/entities/movie.entity.ts
+++ packages/movies/src/movies/entities/movie.entity.ts
@@ -1,1 +1,35 @@
-export class Movie {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Movie {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({
+    name: 'release_date',
+  })
+  releaseDate: string;
+
+  @Column('text', { array: true })
+  genres: string[];
+
+  @Column()
+  certification: string;
+
+  @Column({
+    type: 'int',
+  })
+  runtime: number;
+
+  @Column()
+  thumbnail: string;
+
+  @Column()
+  poster: string;
+}
