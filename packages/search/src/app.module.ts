d6426b0280999b2c3050cd0a18fb3ca447738092
William Queiroz <wnqueiroz@outlook.com>
2022-10-03 21:47:54 -0300

    feat: remove TypeORM setup

--- packages/search/src/app.module.ts
+++ packages/search/src/app.module.ts
@@ -1,28 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
-import { AccountsModule } from './accounts/accounts.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        synchronize: true,
-        entities: [join(__dirname, '**', '*.entity.{ts,js}')],
-        port: 5432,
-        host: configService.get('TYPEORM_HOST'),
-        username: configService.get('TYPEORM_USERNAME'),
-        password: configService.get('TYPEORM_PASSWORD'),
-        database: configService.get('TYPEORM_DATABASE'),
-      }),
-    }),
-    AccountsModule,
-  ],
+  imports: [ConfigModule.forRoot()],
   controllers: [],
   providers: [],
 })
